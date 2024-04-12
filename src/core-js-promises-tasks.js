d725291589cf7de78d0e92c1b5e9f7cf6a23e668
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-04-12 14:45:34 +0300

    feat: last task completed

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -150,8 +150,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = Promise.resolve('');
+  promises.forEach((promise) => {
+    result = result.then((res) => promise.then((value) => res + value));
+  });
+  return result;
 }
 
 module.exports = {
